56350ba43b7f301beed02cb20a43aa2792a81def
msa-sadeghi <sadeghi.msai@gmail.com>
2024-01-04 16:12:46 +0330

    aa
    aa

---
+++ platformer/main.py
@@ -0,0 +1,19 @@
+from pygame.locals import *
+from constants import *
+from world import World
+from levels.level1 import world_data
+
+player_group = pygame.sprite.Group()
+
+game_world = World(world_data, player_group)
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                running = False
+    game_world.draw_bg(SCREEN)
+    player_group.update(game_world.tile_map)
+    player_group.draw(SCREEN)
+    pygame.display.update()
+    CLOCK.tick(FPS)
